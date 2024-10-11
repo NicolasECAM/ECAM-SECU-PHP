fee47f5d7d6337bd56939a2f8b650c855c3f6945
NicolasECAM <143390847+NicolasECAM@users.noreply.github.com>
2024-10-11 13:11:30 +0200

    Create xss.php

---
+++ xss.php
@@ -0,0 +1,19 @@
+<?php
+$texte=$_GET["texte"];
+$texte=htmlspecialchars($texte);
+print "Voici le résultat : ".$texte;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+<h1>Test</h1>
+<form type="get" action="">
+    <input type="text" name="texte" id="texte">
+    <input type="submit" value="go !">
+</form>
+</body>
+</html>
